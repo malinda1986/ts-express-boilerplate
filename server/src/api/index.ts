d9b844a7cc1e327f52be620079f2225ebacdc6f1
Malinda <malindavirtualcapital.co.nz>
2021-06-30 18:37:36 +0530

    initial commit

--- server/src/api/index.ts
+++ server/src/api/index.ts
@@ -1,10 +1,8 @@
 import { Router } from "express";
 import user from "../modules/user/routes";
-import hobby from "../modules/hobby/routes";
 export default () => {
   const app = Router();
   user(app);
-  hobby(app);
 
   return app;
 };
